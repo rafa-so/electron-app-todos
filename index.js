d56ea7d0aa5aac958e18d2974b080d92a17fa204
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-04-10 18:03:47 -0300

    Create template menu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -13,4 +13,13 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate =[
+  {
+    label: 'File'
+  }
+]
