5b2ead7224672252e6937bc1e9c082e1da283203
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-04-10 18:32:18 -0300

    Fix show submenu and added function within menu item quit

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ const menuTemplate =[
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' }
+      { label: 'New Todo' },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
     ]
   }
 ]
+
+if(process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
